bc0b63fb19713609dd51e16d8b7edfb3b9d1ea2a
Gin3601 <laishunliu23@gmail.com>
2026-05-25 09:47:51 +0800

    修改创建MASK节点端口

--- mask/mask_create.py
+++ mask/mask_create.py
@@ -27,7 +27,7 @@ def _resolve_batch_for_mask_creation(input_mask) -> int:
 class LLSSimpleMaskCreate:
     CATEGORY = "LLS/Mask"
     FUNCTION = "create_mask"
-    RETURN_TYPES = ("MASK", "IMAGE", MASK_INFO_TYPE)
+    RETURN_TYPES = ("IMAGE", "MASK", MASK_INFO_TYPE)
     RETURN_NAMES = ("mask_image", "mask", "area_info")
     DESCRIPTION = "Create a geometric mask image from width, height, and shape parameters."
 
